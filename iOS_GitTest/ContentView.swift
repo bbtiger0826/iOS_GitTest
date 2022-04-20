2f0625c73903baffbbf061870fc39489e5b9ed1a
陳柏文 <bbtiger0826@gmail.com>
2022-04-20 19:32:49 +0800

    test

---
+++ iOS_GitTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS_GitTest
+//
+//  Created by 陳柏文 on 2022/4/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS_GitTest/iOS_GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_GitTestApp.swift
+//  iOS_GitTest
+//
+//  Created by 陳柏文 on 2022/4/20.
+//
+
+import SwiftUI
+
+@main
+struct iOS_GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
